cddd42cd1583c86964b5167ee70728c6c7701d9a
leonardomartinez <leonardomartinez@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-18 17:24:27 +0000

    git-svn-id: https://plugins.svn.wordpress.org/simple-wordpress-framework/trunk@164633 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ deactivate.php
@@ -0,0 +1,36 @@
+<?php
+/*
+
+This file handles deactivation of swpframework.  Deletes/Removes database Tables and Options.
+If the application notices that the current version is different than the previous version
+data will not be removed.  Instead the application will assume that an upgrade is in progress.
+
+*/
+
+function swpframework_deactivate()
+{
+  	global $wpdb;
+  	
+  	
+  	## If the Version Numbers are different, this means that user deactivated and then re-activated swpframework 
+  	## to preform an upgrade. Data Tables and Options should not be removed. 
+
+ 	if ((get_option( 'swpframework_version' ) == MYPLUGIN_VERSION ) && ((get_option( 'swpframework_purge_data' )))) 
+ 	{
+  		
+	$table_name = $wpdb->prefix . 'swpframework';  # assigns name to database table.
+
+  	if ($wpdb->get_var("SHOW TABLES LIKE '".$table_name."'") == $table_name) {
+
+  		$wpdb->query( "DROP TABLE ".$table_name."" );
+
+  	}
+  	
+	swpframework_remove_options();
+	
+ 	}
+ 	
+}
+ 
+
+?>
