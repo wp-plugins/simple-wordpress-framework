30b1f81dc6e876e84b3c3de4dcc699acf8ae1f22
leonardomartinez <leonardomartinez@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-18 17:43:41 +0000

    git-svn-id: https://plugins.svn.wordpress.org/simple-wordpress-framework/trunk@164641 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/*
+add_action('admin_head', 'swpframework_load_icons_css');
+
+function swpframework_load_icons_css()
+{
+	if ( strpos($_SERVER['REQUEST_URI'], 'swpframework-options' ) !== false ) { # load css for options page
+		echo '<link rel="stylesheet" href="'.plugins_url('css/swpframework.css', __FILE__).'" type="text/css" media="screen" />'."\n";
+	}
+
+}
+*/
+
+?>
